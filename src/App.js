935bf1059df333aae5737f21ddd21c68f456eb7e
박성윤 <bagseong-yun@bagseong-yun-ui-MacBookPro.local>
2020-08-23 05:43:54 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
 
   return (
-    <Wrapper>
-      {/*<> Fragment 감싸는 상황에서 사용 가능 </>*/}
-      <Hello name="React" color="red" isSpecial />   {/* isSpecial = {true} === inSpecial 이름만 넣어줘도 true랑 같은 의미 */}
-      <Hello color="pink" />
-    </Wrapper>
+    <Counter />
   );
 }
 
