21c59f9a566a86bd4d092af0a04d2d42c3f792d5
박성윤 <bagseong-yun@bagseong-yun-ui-MacBookPro.local>
2020-08-23 12:35:05 +0900

    useRef

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
         name: '',
         nickname: ''
     });
+    const nameInput = useRef();
 
     const { name, nickname } = inputs;    // 비구조화 할당을 통해 값 추출
 
@@ -20,13 +21,25 @@ function InputSample() {
         setInputs({
             name: '',
             nickname: '',
-        })
+        });
+        nameInput.current.focus();  // 여기서 current는 DOM 엘리먼트 객체를 가리킨다.
     };
 
     return (
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
-            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
+            <input
+                name="name"
+                placeholder="이름"
+                onChange={onChange}
+                value={name}
+                ref={nameInput}
+            />
+            <input
+                name="nickname"
+                placeholder="닉네임"
+                onChange={onChange}
+                value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
@@ -36,3 +49,10 @@ function InputSample() {
     );
 }
 export default InputSample;
+
+/*
+JS에서는 특정 DOM을 선택할 때 getElementById, querySelector, DOM Selector함수를 사용하지만
+리액트를 사용할 때는 ref 라는 것을 사용한다.
+ref를 사용할 때는 useRef라는 Hook 함수를 사용하는데 초기화 버튼을 눌렀을 때 이름 input에 포커스가 잡히도록
+useRef 를 사용하여 기능을 구현하는 코드이다.
+*/
