14d2d558bb53399dc1fcfa55e56c7a4c0bfd2b6f
박성윤 <bagseong-yun@bagseong-yun-ui-MacBookPro.local>
2020-08-23 18:41:05 +0900

    CRUD

--- src/UserList.js
+++ src/UserList.js
@@ -1,36 +1,34 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b
+                style={{
+                    cursor: 'pointer',
+                    color: user.active ? 'green' : 'black'
+                }}
+                onClick={() => onToggle(user.id)}
+            >
+                {user.username}
+            </b>
+            <span>({user.email})</span>
+
+            <button onClick={() => onRemove(user.id)}>삭제</button> {/* id가 __인 객체를 삭제해라 */}
         </div>
-    )
+    );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'sungyun',
-            email: 'sungyun@gmail.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
-
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {users.map(user => (
-                <User user={user} key={user.id} />
+                <User
+                    user={user}
+                    key={user.id}
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
             ))}
         </div>
     );
